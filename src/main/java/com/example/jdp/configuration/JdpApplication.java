1a08a7411c649153442bf48901d48c1ee153675e
Sanatan Das <sanatanonline@gmail.com>
2022-06-30 22:27:29 -0700

    code updated

---
+++ src/main/java/com/example/jdp/configuration/JdpApplication.java
@@ -0,0 +1,12 @@
+package com.example.jdp.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JdpApplication.class, args);
+  }
+}
---
+++ src/test/java/com/example/jdp/configuration/JdpApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.jdp.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdpApplicationTest {
+
+  @Test
+  void contextLoads() {}
+}
